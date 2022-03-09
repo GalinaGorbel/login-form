c5ced942bfcddb6e77fc7e67e0b599dff1957bc2
Галина Горбель <galinagorbel@gmail.com>
2022-03-09 16:47:29 +0300

    created project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { LoginFormComponent } from './forms/login-form/login-form.component';
+import { SignUpFormComponent } from './forms/sign-up-form/sign-up-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginFormComponent,
+    SignUpFormComponent
   ],
   imports: [
     BrowserModule
