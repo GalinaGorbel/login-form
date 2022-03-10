c504add77bf8e7b5e350864651c1a35956a2d9c5
Галина Горбель <galinagorbel@gmail.com>
2022-03-10 15:45:30 +0300

    добавила проверку на ширину экрана

--- src/app/browser-info.service.ts
+++ src/app/browser-info.service.ts
@@ -8,6 +8,6 @@ export class BrowserInfoService {
   constructor(private platform: Platform) {}
 
   isMobile(): boolean {
-    return this.platform.ANDROID || this.platform.IOS;
+    return this.platform.ANDROID || this.platform.IOS || window.innerWidth < 1000;
   }
 }
